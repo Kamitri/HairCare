c604c087c051a49c7f72b1f26926b19118e7dd9e
Kamitri <mtri2003@gmail.com>
2023-11-11 17:12:16 +0700

    can now change quantity in the cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,6 +3,7 @@ import { Col, Container, Form, Image, Row, Table } from 'react-bootstrap'
 import CartContext from '../CartContext';
 import allProducts from '../../assets/json/Products.json'
 import { Link } from 'react-router-dom';
+import './index.scss'
 
 
 function CustomerInfo({ total }) {
@@ -17,7 +18,7 @@ function CustomerInfo({ total }) {
         alert(msg);
     }
     return (
-        <Form class="container needs-validation" onSubmit={handleFormSubmit}>
+        <Form className="container" onSubmit={handleFormSubmit}>
             <Row className='mb-3'>
                 <Col md={4}>
                     <Form.Label for="firstName">First name (*)</Form.Label>
@@ -45,14 +46,14 @@ function CustomerInfo({ total }) {
             </Row>
             <Row className='my-3'>
                 <Col>
-                    <button class="btn btn-primary" type="submit">Submit form</button>
+                    <button className="btn-stylized rounded px-2 py-1" type="submit">Submit form</button>
                 </Col>
             </Row>
         </Form>
     )
 }
 
-function CartTable({ shoppingCart, total, setTotal }) {
+function CartTable({ shoppingCart, setShoppingCart, total, setTotal }) {
     
     useEffect(() => {
         let newTotal = 0;
@@ -60,9 +61,23 @@ function CartTable({ shoppingCart, total, setTotal }) {
             const productInfo = shoppingCart[index];
             const product = allProducts.filter((product) => product.id === productInfo.id)[0];
             newTotal += product.price * productInfo.quantity;
-            setTotal(newTotal);
+            setTotal(Number(newTotal).toFixed(2));
         }
 	  }, [shoppingCart]);
+
+    function handleChangeQuantity(productId, newQuantity) {
+        let newShoppingCart = [...shoppingCart];
+        // Check if the cart already has this item. If it does, increment instead of pushing
+
+        for (let index = 0; index < newShoppingCart.length; index++) {
+            const element = newShoppingCart[index];
+            if (element.id === productId) {
+                newShoppingCart[index].quantity = newQuantity;
+            } 
+            
+        }
+        setShoppingCart(newShoppingCart);
+    }
 
     return (
     <Table responsive hover>
@@ -84,7 +99,7 @@ function CartTable({ shoppingCart, total, setTotal }) {
                     <Image style={{maxHeight: '18vh'}} fluid src={ allProducts.filter((product) => product.id === productInfo.id)[0].images[0] }>
                     </Image>
                 </td>
-                <td>{ productInfo.quantity }</td>
+                <td><input style={{maxWidth: '50px'}} min={0} max={allProducts.filter((product) => product.id === productInfo.id)[0].inStock} type='number' value={ productInfo.quantity } onChange={(e) => handleChangeQuantity(productInfo.id, parseInt(e.target.value))} /></td>
                 <td>{ '$' + parseFloat(allProducts.filter((product) => product.id === productInfo.id)[0].price).toFixed(2) }</td>
             </tr>
             )}
@@ -101,13 +116,13 @@ function CartTable({ shoppingCart, total, setTotal }) {
 
 
 function Cart() {
-    const { shoppingCart } = useContext(CartContext);
+    const { shoppingCart, setShoppingCart } = useContext(CartContext);
     const [ total, setTotal ] = useState(0);
     return (
         <Container className='mt-4'>
             {shoppingCart.length > 0 ?
              <Container fluid>
-                <CartTable shoppingCart={shoppingCart} total={total} setTotal={setTotal} />
+                <CartTable shoppingCart={shoppingCart} setShoppingCart={setShoppingCart} total={total} setTotal={setTotal} />
                 <h1 className='mt-5'>Your contact information</h1>
                 <CustomerInfo total={total}/>
             </Container>
