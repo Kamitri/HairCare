aa1cb28b35506cfbbf0009c7584ca476d18f70ab
Kamitri <mtri2003@gmail.com>
2023-11-12 18:16:57 +0700

    fix pagination first button show as disabled

--- src/components/AdvancedPagination/AdvancedPagination.jsx
+++ src/components/AdvancedPagination/AdvancedPagination.jsx
@@ -3,20 +3,24 @@ import './index.scss'
 import { Pagination, Stack } from 'react-bootstrap'
 
 function AdvancedPagination({ currentPage, setCurrentPage, maxPage }) {
+    function setCurrentPageAndScrollToTop(page) {
+        setCurrentPage(page);
+        window.scrollTo(0, 0);
+    }
     return (
     <Pagination>
         <Stack direction='horizontal' className='me-5 ms-auto'>
-            <Pagination.First disabled={(currentPage <= 2) ? true : false} onClick={() => setCurrentPage(1)} />
-            <Pagination.Prev disabled={(currentPage <= 1) ? true : false} onClick={() => setCurrentPage(currentPage - 1)} />
+            <Pagination.First disabled={(currentPage <= 1) ? true : false} onClick={() => setCurrentPageAndScrollToTop(1)} />
+            <Pagination.Prev disabled={(currentPage <= 1) ? true : false} onClick={() => setCurrentPageAndScrollToTop(currentPage - 1)} />
         </Stack>
-        {currentPage > 2 && <Pagination.Item onClick={() => setCurrentPage(currentPage - 2)}>{currentPage - 2}</Pagination.Item>}
-        {currentPage > 1 && <Pagination.Item onClick={() => setCurrentPage(currentPage - 1)}>{currentPage - 1}</Pagination.Item>}
+        {currentPage > 2 && <Pagination.Item onClick={() => setCurrentPageAndScrollToTop(currentPage - 2)}>{currentPage - 2}</Pagination.Item>}
+        {currentPage > 1 && <Pagination.Item onClick={() => setCurrentPageAndScrollToTop(currentPage - 1)}>{currentPage - 1}</Pagination.Item>}
         <Pagination.Item active>{currentPage}</Pagination.Item>
-        {maxPage - currentPage > 0 && <Pagination.Item onClick={() => setCurrentPage(currentPage + 1)}>{currentPage + 1}</Pagination.Item>}
-        {maxPage - currentPage > 1 && <Pagination.Item onClick={() => setCurrentPage(currentPage + 2)}>{currentPage + 2}</Pagination.Item>}
+        {maxPage - currentPage > 0 && <Pagination.Item onClick={() => setCurrentPageAndScrollToTop(currentPage + 1)}>{currentPage + 1}</Pagination.Item>}
+        {maxPage - currentPage > 1 && <Pagination.Item onClick={() => setCurrentPageAndScrollToTop(currentPage + 2)}>{currentPage + 2}</Pagination.Item>}
         <Stack direction='horizontal' className='ms-5 me-auto'>
-            <Pagination.Next disabled={(maxPage === currentPage) ? true : false} onClick={() => setCurrentPage(currentPage + 1)} />
-            <Pagination.Last disabled={(maxPage === currentPage) ? true : false} onClick={() => {setCurrentPage(maxPage)}} />
+            <Pagination.Next disabled={(maxPage === currentPage) ? true : false} onClick={() => setCurrentPageAndScrollToTop(currentPage + 1)} />
+            <Pagination.Last disabled={(maxPage === currentPage) ? true : false} onClick={() => {setCurrentPageAndScrollToTop(maxPage)}} />
         </Stack>
     </Pagination>
     )
